473723bab018132e7296bf742674dd821ada16b2
Sbscodes <55776048+sbscodes@users.noreply.github.com>
2020-04-22 11:45:08 +0530

    Create Hello.py

---
+++ 01_Getting Started/Hello.py
@@ -0,0 +1,7 @@
+'''
+This is Your First Python Program
+--->Sb's Codes<---
+Follow On Instagram--->www.instagram.com/sb_s_codes
+If Any Query Regarding Python Mail-->sbscodes@gmail.co
+'''
+print("Hello World")
